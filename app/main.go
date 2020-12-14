98c35da7a86c2154b734218766bc3879a1a3077c
heyar341 <kityonya@gmail.com>
2020-12-14 22:43:28 +0900

    ルート検索をdirectionのmainで行うように変更

--- app/main.go
+++ app/main.go
@@ -17,58 +17,25 @@
 package main
 
 import (
-	"context"
-	"flag"
-	"fmt"
-	"log"
-	"os"
+	"encoding/json"
+	//"fmt"
+	//"log"
+	"net/http"
+	//"os"
 	"app/direction"
-	//envファイル操作用のパッケージ
-	"github.com/joho/godotenv"
 
-	"github.com/kr/pretty"
-	"googlemaps.github.io/maps"
 )
 
-var apiKey = flag.String("key", "", "API Key for using Google Maps API.")
-
-func usageAndExit(msg string) {
-	fmt.Fprintln(os.Stderr, msg)
-	fmt.Println("Flags:")
-	flag.PrintDefaults()
-	os.Exit(2)
-}
-
-func check(err error) {
-	if err != nil {
-		log.Fatalf("fatal error: %s", err)
-	}
-}
-
 func main() {
-	//API呼び出しの準備
-	env_err := godotenv.Load("env/dev.env")
-	if env_err != nil{
-		panic("Can't load env file")
-	}
-	*apiKey = os.Getenv("MAP_API_KEY")
-	flag.Parse()
 
-	var client *maps.Client
-	var err error
-	if *apiKey != "" {
-		client, err = maps.NewClient(maps.WithAPIKey(*apiKey), maps.WithRateLimit(2))
-	} else {
-		usageAndExit("Please specify an API Key, or Client ID and Signature.")
-	}
-	check(err)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
 	//Direction API
-	r := direction.MakeRequest(Origin,Destination,DepartureTime)
-	routes, waypoints, err := client.Directions(context.Background(), r)
-	check(err)
-	pretty.Println(waypoints)
-	pretty.Println(routes)
+	http.HandleFunc("/route_search",searchRoute)
+	http.ListenAndServe(":80",nil)
 }
 
-
-
+func searchRoute(w http.ResponseWriter, req *http.Request)  {
+	routes := direction.SearchRoute(req)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(routes)
+}
