1a1114c2596eea55e3ac3e50616da4df2cb8a11e
heyar341 <kityonya@gmail.com>
2020-12-19 20:13:26 +0900

    ajax通信でbackendからのResponseを取得して、レンダリングするように変更

--- app/test/place_and_direction.js
+++ app/test/place_and_direction.js
@@ -84,57 +84,95 @@ class AutocompleteDirectionsHandler {
             return;
         }
         const me = this;
-        this.directionsService.route(
-            {
-                origin: { placeId: this.originPlaceId },
-                destination: { placeId: this.destinationPlaceId },
-                travelMode: this.travelMode,
-                //↓複数ルートを返す場合、指定
-                // provideRouteAlternatives: true,
-            },
-            (response, status) => {
-                if (status === "OK") {
-                    //複数ルートがある場合、subRouteRendererで各ルートを薄く表示
-                    if(response.routes.length > 1) {
-                        for (var i = 0; i < response.routes.length; i++) {
-                            //jsではObjectは参照渡しなので、Object.assignを使って、値渡しにする
-                            var sub_res = Object.assign({}, response);
-                            sub_res.routes = [response.routes[i]];
-                            // 各ルートごとに、Renderオブジェクトを作成する必要がある
-                            var subRouteRenderer = new google.maps.DirectionsRenderer();
-                            //ドキュメントURL: https://developers.google.com/maps/documentation/javascript/reference/directions#DirectionsRendererOptions
-                            subRouteRenderer.setOptions({
-                                suppressMarkers: false,
-                                suppressPolylines: false,
-                                suppressInfoWindows: false,
-                                //Colorとopacity(不透明度)と太さを設定
-                                polylineOptions: {
-                                    strokeColor: '#808080',
-                                    strokeOpacity: 0.5,
-                                    strokeWeight: 7
-                                }
-                            });
-                            subRouteRenderer.setDirections(sub_res)
-                            subRouteRenderer.setMap(this.map)
-                        }
-                    }
-                    //responseをRendererに渡して、ルートを描画
-                    //ドキュメントURL: https://developers.google.com/maps/documentation/javascript/reference/marker#MarkerOptions
-                    me.directionsRenderer.setOptions({
-                            suppressMarkers: false,
-                            suppressPolylines: false,
-                            suppressInfoWindows: false,
-                            polylineOptions: {
-                                strokeColor: '#00bfff',
-                                strokeOpacity: 1.0,
-                                strokeWeight: 7
-                            }
-                        });
-                    me.directionsRenderer.setDirections(response);
-                } else {
-                    window.alert("Directions request failed due to " + status);
+        var data = {
+            origin: { placeId: this.originPlaceId },
+            destination: { placeId: this.destinationPlaceId },
+            travelMode: this.travelMode,
+            //↓複数ルートを返す場合、指定
+            // provideRouteAlternatives: true,
+        };
+        $.ajax({
+            url:"http://localhost:8080/route_search", // 通信先のURL
+            type:"POST",		// 使用するHTTPメソッド
+            data:"origin=place_id:" + encodeURIComponent(this.originPlaceId) +
+                "&destination=place_id:" + encodeURIComponent(this.destinationPlaceId) +
+                "&travelMode=" + encodeURIComponent(this.travelMode), // 送信するデータ
+            dataType:"json", // 応答のデータの種類
+            // (xml/html/script/json/jsonp/text)
+            timespan:1000 		// 通信のタイムアウトの設定(ミリ秒)
+
+            // 2. doneは、通信に成功した時に実行される
+            //  引数のdata1は、通信で取得したデータ
+            //  引数のtextStatusは、通信結果のステータス
+            //  引数のjqXHRは、XMLHttpRequestオブジェクト
+        }).done(function(data1,textStatus,jqXHR) {
+            me.directionsRenderer.setOptions({
+                suppressMarkers: false,
+                suppressPolylines: false,
+                suppressInfoWindows: false,
+                polylineOptions: {
+                    strokeColor: '#00bfff',
+                    strokeOpacity: 1.0,
+                    strokeWeight: 7
                 }
-            }
-        );
+            });
+            console.log(data1)
+            me.directionsRenderer.setDirections(data1);
+        });
+        // this.directionsService.route(
+        //     {
+        //         origin: { placeId: this.originPlaceId },
+        //         destination: { placeId: this.destinationPlaceId },
+        //         travelMode: this.travelMode,
+        //         //↓複数ルートを返す場合、指定
+        //         // provideRouteAlternatives: true,
+        //     },
+        //     (response, status) => {
+        //         if (status === "OK") {
+        //             //複数ルートがある場合、subRouteRendererで各ルートを薄く表示
+        //             if(response.routes.length > 1) {
+        //                 for (var i = 0; i < response.routes.length; i++) {
+        //                     //jsではObjectは参照渡しなので、Object.assignを使って、値渡しにする
+        //                     var sub_res = Object.assign({}, response);
+        //                     sub_res.routes = [response.routes[i]];
+        //                     // 各ルートごとに、Renderオブジェクトを作成する必要がある
+        //                     var subRouteRenderer = new google.maps.DirectionsRenderer();
+        //                     //ドキュメントURL: https://developers.google.com/maps/documentation/javascript/reference/directions#DirectionsRendererOptions
+        //                     subRouteRenderer.setOptions({
+        //                         suppressMarkers: false,
+        //                         suppressPolylines: false,
+        //                         suppressInfoWindows: false,
+        //                         //Colorとopacity(不透明度)と太さを設定
+        //                         polylineOptions: {
+        //                             strokeColor: '#808080',
+        //                             strokeOpacity: 0.5,
+        //                             strokeWeight: 7
+        //                         }
+        //                     });
+        //                     subRouteRenderer.setDirections(sub_res)
+        //                     subRouteRenderer.setMap(this.map)
+        //                 }
+        //             }
+        //             //responseをRendererに渡して、ルートを描画
+        //             //ドキュメントURL: https://developers.google.com/maps/documentation/javascript/reference/marker#MarkerOptions
+        //             me.directionsRenderer.setOptions({
+        //                     suppressMarkers: false,
+        //                     suppressPolylines: false,
+        //                     suppressInfoWindows: false,
+        //                     polylineOptions: {
+        //                         strokeColor: '#00bfff',
+        //                         strokeOpacity: 1.0,
+        //                         strokeWeight: 7
+        //                     }
+        //                 });
+        //             this.resp = response;
+        //             console.log(response)
+        //             console.log(typeof response)
+        //             me.directionsRenderer.setDirections(response);
+        //         } else {
+        //             window.alert("Directions request failed due to " + status);
+        //         }
+        //     }
+        // );
     }
 }
