962e983c17cbdf1280b9ed3343469b0aa9d4b637
heyar341 <kityonya@gmail.com>
2021-01-11 22:48:13 +0900

    Delete機能をdbhandlerのdelete_handlerで行うように変更

---
+++ app/dbhandler/delete_handler.go
@@ -0,0 +1,27 @@
+package dbhandler
+
+import (
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+)
+
+func Delete(dbName,collectionName string, document interface{}) (error){
+	//objectIDを取得するには、１番目の帰り値のInsertedIDフィールドを取得する
+	client, ctx, err := connectDB()
+	if err != nil {
+		return err
+	}
+	//処理終了後に切断
+	defer client.Disconnect(ctx)
+	database := client.Database(dbName)
+	collection := database.Collection(collectionName)
+	//DBからのレスポンスを挿入する変数
+	var deletedDocument bson.M
+	err = collection.FindOneAndDelete(ctx,document).Decode(&deletedDocument)
+	if err != nil && err == mongo.ErrNoDocuments {
+			log.Fatalln("During deleting a document: ",err)
+			return err
+		}
+	return nil
+}
