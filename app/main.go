4f6321c8178657af60fc097cde65757258af7219
heyar341 <kityonya@gmail.com>
2021-02-25 08:46:36 +0900

    保存したルートの確認編集画面を表示できるよう変更

--- app/main.go
+++ app/main.go
@@ -12,14 +12,15 @@ import (
 	"app/controllers/envhandler"
 
 )
-var tpl, auth_tpl, home_tpl, simul_search_tpl, mypage_tpl *template.Template
+var tpl, auth_tpl, home_tpl, simul_search_tpl, mypage_tpl,show_route_tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.Must(template.ParseGlob("templates/multi_search/*")).ParseGlob("templates/includes/*.html"))
+	tpl = template.Must(template.Must(template.ParseGlob("templates/multi_search/search/*")).ParseGlob("templates/includes/*.html"))
 	simul_search_tpl = template.Must(template.Must(template.ParseGlob("templates/simul_search/*")).ParseGlob("templates/includes/*.html"))
 	auth_tpl = template.Must(template.Must(template.ParseGlob("templates/auth/*")).ParseGlob("templates/includes/*.html"))
 	home_tpl = template.Must(template.Must(template.ParseGlob("templates/home/home.html")).ParseGlob("templates/includes/*.html"))
 	mypage_tpl = template.Must(template.Must(template.ParseGlob("templates/mypage/*.html")).ParseGlob("templates/includes/*.html"))
+	show_route_tpl = template.Must(template.Must(template.ParseGlob("templates/multi_search/show_and_edit/*")).ParseGlob("templates/includes/*.html"))
 
 }
 
@@ -43,6 +44,8 @@ func main() {
 	http.HandleFunc("/routes_save",routes.SaveRoutes)
 	http.HandleFunc("/simul_search",simulSearchTpl)
 	http.HandleFunc("/do_simul_search",routes.DoSimulSearch)
+	http.HandleFunc("/show_route/",showAndEditRoutes)
+
 	http.HandleFunc("/",home)
 
 	http.HandleFunc("/mypage", mypage)
@@ -103,4 +106,15 @@ func simulSearchTpl(w http.ResponseWriter, req *http.Request) {
 	nineIterator := []int {1,2,3,4,5,6,7,8,9}
 	data := map[string]interface{}{"apiKey":apiKey,"isLoggedIn":isLoggedIn,"nineIterator":nineIterator}
 	simul_search_tpl.ExecuteTemplate(w, "simul_search.html",data)
+}
+
+func showAndEditRoutes(w http.ResponseWriter, req *http.Request){
+//envファイルからAPIキー取得
+apiKey := envhandler.GetEnvVal("MAP_API_KEY")
+isLoggedIn := auth.IsLoggedIn(req)
+route_title := req.URL.Query().Get("route_title")
+userID, _ := auth.GetLoginUserID(req)
+routeInfo := routes.GetRoute(w,route_title, userID)
+data := map[string]interface{}{"apiKey":apiKey,"isLoggedIn":isLoggedIn,"routeInfo":routeInfo}
+show_route_tpl.ExecuteTemplate(w, "multi_route_show.html", data)
 }
