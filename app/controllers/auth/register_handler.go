a6b24ca006bd802adb9165979cdb7254a0a7878a
heyar341 <kityonya@gmail.com>
2021-02-24 16:45:05 +0900

    メールアドレスが使用可能かチェックする機能作成

--- app/controllers/auth/register_handler.go
+++ app/controllers/auth/register_handler.go
@@ -2,12 +2,15 @@ package auth
 
 import (
 	"app/dbhandler"
+	"encoding/json"
 	"fmt"
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/smtp"
@@ -198,4 +201,48 @@ func ConfirmRegister(w http.ResponseWriter, req *http.Request) {
 	}
 	http.SetCookie(w, c)
 	http.Redirect(w, req, "/", http.StatusSeeOther)
+}
+
+func EmailIsAvailable(w http.ResponseWriter, req *http.Request) {
+	//メールアドレスが使用可能かのリクエスト
+	type ValidEmailRequest struct {
+		Email string `json:"email"`
+	}
+
+	if req.Method != "POST" {
+		http.Error(w, "HTTPメソッドが不正です。", http.StatusBadRequest)
+		return
+	}
+	//requestのフィールドを保存する変数
+	var reqFields ValidEmailRequest
+	body, _ := ioutil.ReadAll(req.Body)
+	err := json.Unmarshal(body, &reqFields)
+	if err != nil {
+		http.Error(w, "入力に不正があります。", http.StatusBadRequest)
+		log.Printf("Error while json marshaling: %v", err)
+		return
+	}
+
+	//メールアドレスが使用可能か入れる変数
+	var isValid = false
+	emailDoc := bson.D{{"email", reqFields.Email}}
+	//DBから取得
+	_, err = dbhandler.Find("googroutes", "users", emailDoc, nil)
+	//ドキュメントがない場合、メールアドレスは使用可能
+	if err == mongo.ErrNoDocuments {
+		isValid = true
+	}
+
+	//レスポンス作成
+	w.Header().Set("Content-Type", "application/json")
+	type ResponseMsg struct {
+		Valid bool `json:"valid"`
+	}
+	msg := ResponseMsg{Valid: isValid}
+	respJson, err := json.Marshal(msg)
+	if err != nil {
+		log.Printf("Error while json marshaling: %v", err)
+	}
+	w.Write(respJson)
+
 }
