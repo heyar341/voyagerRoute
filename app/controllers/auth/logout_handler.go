5b4b8504d9a1832adfbe9ccb25482616b3742c68
heyar341 <kityonya@gmail.com>
2021-02-26 19:00:09 +0900

    エラーメッセージを変更

--- app/controllers/auth/logout_handler.go
+++ app/controllers/auth/logout_handler.go
@@ -13,15 +13,6 @@ func Logout(w http.ResponseWriter, req *http.Request) {
 	}
 	//Cookieからセッション情報取得
 	c, err := req.Cookie("sessionId")
-	//Cookieが設定されてない場合
-	if err != nil {
-		c = &http.Cookie{
-			Name:  "sessionId",
-			Value: "",
-		}
-	}
-
-	sesId, err := ParseToken(c.Value)
 	if err != nil {
 		msg := "ログインしていません"
 		http.Redirect(w, req, "/?msg="+msg, http.StatusSeeOther)
@@ -29,20 +20,27 @@ func Logout(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	if sesId != "" {
-		//DBから読み込み
-		sesDoc := bson.D{{"session_id", sesId}}
-		err = dbhandler.Delete("googroutes", "sessions", sesDoc)
-		if err != nil {
-			msg := "エラ〜が発生しました。"
-			http.Redirect(w, req, "/?msg="+msg, http.StatusSeeOther)
-			return
-		}
+	sessionID, err := ParseToken(c.Value)
+	if err != nil {
+		msg := "ログインしていません"
+		http.Redirect(w, req, "/?msg="+msg, http.StatusSeeOther)
+		log.Println(err)
+		return
 	}
 
+	//DBから読み込み
+	sesDoc := bson.D{{"session_id", sessionID}}
+	err = dbhandler.Delete("googroutes", "sessions", sesDoc)
+	if err != nil {
+		msg := "ログアウト中エラーが発生しました。"
+		http.Redirect(w, req, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	//Cookieを削除
 	c.MaxAge = -1
 	http.SetCookie(w, c)
-	msg := "ログアウトしました"
-	http.Redirect(w, req, "/?msg="+msg, http.StatusSeeOther)
 
+	success := "ログアウトしました"
+	http.Redirect(w, req, "/?msg="+success, http.StatusSeeOther)
 }
