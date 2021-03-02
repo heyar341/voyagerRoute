ba66e04a9f60f4853f85eb0822d357a0781b51cd
heyar341 <kityonya@gmail.com>
2021-03-02 17:20:27 +0900

    ユーザー情報編集用テンプレートファイルの操作機能追加

--- app/controllers/profile/tpl_handler.go
+++ app/controllers/profile/tpl_handler.go
@@ -36,3 +36,53 @@ func ShowProfile(w http.ResponseWriter, req *http.Request) {
 
 	profileTpl.ExecuteTemplate(w, "profile.html", data)
 }
+
+func EditUserNameForm(w http.ResponseWriter, req *http.Request) {
+	data, ok := req.Context().Value("data").(map[string]interface{})
+	if !ok {
+		msg = "エラ〜が発生しました。もう一度操作しなおしてください。"
+		http.Redirect(w, req, "/mypage/?msg="+msg, http.StatusSeeOther)
+		log.Printf("Error while getting data from context: %v", ok)
+		return
+	}
+	user, ok := req.Context().Value("user").(model.UserData)
+	if !ok {
+		msg = "エラ〜が発生しました。もう一度操作しなおしてください。"
+		http.Redirect(w, req, "/mypage/?msg="+msg, http.StatusSeeOther)
+		log.Printf("Error while getting user from context: %v", ok)
+		return
+	}
+	data["userName"] = user.UserName
+
+	profileTpl.ExecuteTemplate(w, "username_edit.html", data)
+}
+func EditEmailForm(w http.ResponseWriter, req *http.Request) {
+	data, ok := req.Context().Value("data").(map[string]interface{})
+	if !ok {
+		msg = "エラ〜が発生しました。もう一度操作しなおしてください。"
+		http.Redirect(w, req, "/mypage/?msg="+msg, http.StatusSeeOther)
+		log.Printf("Error while getting data from context: %v", ok)
+		return
+	}
+	user, ok := req.Context().Value("user").(model.UserData)
+	if !ok {
+		msg = "エラ〜が発生しました。もう一度操作しなおしてください。"
+		http.Redirect(w, req, "/mypage/?msg="+msg, http.StatusSeeOther)
+		log.Printf("Error while getting user from context: %v", ok)
+		return
+	}
+	data["email"] = user.Email
+
+	profileTpl.ExecuteTemplate(w, "email_edit.html", data)
+}
+func EditPasswordForm(w http.ResponseWriter, req *http.Request) {
+	data, ok := req.Context().Value("data").(map[string]interface{})
+	if !ok {
+		msg = "エラ〜が発生しました。もう一度操作しなおしてください。"
+		http.Redirect(w, req, "/mypage/?msg="+msg, http.StatusSeeOther)
+		log.Printf("Error while getting data from context: %v", ok)
+		return
+	}
+
+	profileTpl.ExecuteTemplate(w, "password_edit.html", data)
+}
