8aa4b628c370c96abc76fe6650cb97c79c06be5d
heyar341 <kityonya@gmail.com>
2021-03-03 15:12:02 +0900

    Multi_RouteTitlesはmarshalを使う場所では必要ないので除外。またSessionDataは不要になったので削除

--- app/model/model.go
+++ app/model/model.go
@@ -2,7 +2,6 @@ package model
 
 import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"time"
 )
 
 type UserData struct {
@@ -10,13 +9,7 @@ type UserData struct {
 	UserName         string               `json:"username" bson:"username"`
 	Email            string               `json:"email" bson:"email"`
 	Password         []byte               `json:"password" bson:"password"`
-	MultiRouteTitles map[string]time.Time `json:"multi_route_titles" bson:"multi_route_titles"`
-}
-
-type SessionData struct {
-	ID        primitive.ObjectID `json:"id" bson:"_id"`
-	SessionId string             `json:"session_id" bson:"session_id"`
-	UserID    primitive.ObjectID `json:"user_id" bson:"user_id"`
+	//MultiRouteTitles map[string]time.Time `json:"multi_route_titles" bson:"multi_route_titles"`
 }
 
 //ルート編集保存requestのフィールドを保存するstruct
