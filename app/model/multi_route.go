ba0d4ed852dd161e682a6ad850354f87a33e10c9
heyar341 <kityonya@gmail.com>
2021-03-08 16:03:37 +0900

    updateで参照されるので、routesの頭文字を　Rに変更。また、UpdateRouteメソッド作成

--- app/model/multi_route.go
+++ app/model/multi_route.go
@@ -6,21 +6,32 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type route interface {
+type Route interface {
 }
 
 type MultiRoute struct {
 	ID     primitive.ObjectID `json:"_id" bson:"_id"`
-	Title  string           `json:"title" bson:"title"`
-	Routes map[string]route `json:"routes" bson:"routes"`
+	Title  string             `json:"title" bson:"title"`
+	Routes map[string]Route   `json:"routes" bson:"routes"`
 }
 
-func SaveRoute(userID primitive.ObjectID, multiRoute *MultiRoute) error {
+func (m *MultiRoute) SaveRoute(userID primitive.ObjectID) error {
 	routeDocument := bson.D{
 		{"user_id", userID},
-		{"title", multiRoute.Title},
-		{"routes", multiRoute.Routes},
+		{"title", m.Title},
+		{"routes", m.Routes},
 	}
 	_, err := dbhandler.Insert("googroutes", "routes", routeDocument)
+	return err
+}
+
+func (m *MultiRoute) UpdateRoute() error{
+	//routes collectionに保存
+	routeDoc := bson.M{"_id": m.ID}
+	updateDoc := bson.D{
+		{"title", m.Title},
+		{"routes", m.Routes},
+	}
+	err := dbhandler.UpdateOne("googroutes", "routes", "$set", routeDoc, updateDoc)
 	return err
 }
