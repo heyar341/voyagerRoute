1570e70838f8bbdaf1158f41db09f6d6ef8089e4
heyar341 <kityonya@gmail.com>
2021-03-08 16:05:03 +0900

    multi_search.goと同様、可読性を改善し、エラーハンドリングを１カ所に集約

--- app/controllers/multiroute/update_route.go
+++ app/controllers/multiroute/update_route.go
@@ -1,86 +1,118 @@
 package multiroute
 
 import (
-	"app/dbhandler"
 	"app/model"
+	"app/customerr"
 	"encoding/json"
-	"go.mongodb.org/mongo-driver/bson"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"log"
 	"net/http"
 	"strings"
+	"time"
 )
+
+//ルート編集保存requestのフィールドを保存するstruct
+type RouteUpdateRequest struct {
+	ID            primitive.ObjectID     `json:"_id" bson:"_id"`
+	Title         string                 `json:"title" bson:"title"`
+	PreviousTitle string                 `json:"previous_title" bson:"previous_title"`
+	Routes        map[string]model.Route `json:"routes" bson:"routes"`
+}
+
+func getUpdateRoutesInfo(req *http.Request) (*routesData, string) {
+	reqFields, ok := req.Context().Value("reqFields").(RouteUpdateRequest)
+	if !ok {
+		return &routesData{
+			err: customerr.BaseErr{
+				Op:  "Get req routes info from context",
+				Msg: "エラーが発生しました。",
+				Err: fmt.Errorf("error while getting request fields from reuest's context"),
+			},
+		}, ""
+	}
+	return &routesData{
+		routesInfo: model.MultiRoute{
+			ID:     reqFields.ID,
+			Title:  reqFields.Title,
+			Routes: reqFields.Routes,
+		},
+		err: nil,
+	}, reqFields.PreviousTitle
+}
+
+func (r *routesData) updateRoute() {
+	if r.err != nil {
+		return
+	}
+	err := r.routesInfo.UpdateRoute()
+	if err != nil {
+		e := customerr.BaseErr{
+			Op:  "Save new multi route",
+			Err: fmt.Errorf("error while updating multi route: %w", err),
+		}
+
+		if strings.Contains(err.Error(), "(BSONObjectTooLarge)") {
+			e.Msg = "ルートのデータサイズが大きすぎるため、保存できません。\nルートの分割をお願いします。"
+			r.err = e
+		} else {
+			e.Msg = "エラーが発生しました。"
+			r.err = e
+		}
+	}
+}
+
+func (r *routesData) updateRouteTitles(pTitle string) {
+	if r.err != nil {
+		return
+	}
+	now := time.Now().UTC() //MongoDBでは、timeはUTC表記で扱われ、タイムゾーン情報は入れられない
+	if r.routesInfo.Title != pTitle {
+		//「元のルート名をuser documentから削除」
+		//documentではなく、document内のフィールドを削除する場合、Deleteではなく、Update operatorの$unsetを使って削除する
+		//公式ドキュメントURL: https://docs.mongodb.com/manual/reference/operator/update/unset/
+		err := model.UpdateMultiRouteTitles(r.userID, pTitle, "$unset", "")
+		if err != nil {
+			r.err = customerr.BaseErr{
+				Op:  "Remove previous multi route title",
+				Msg: "エラーが発生しました。",
+				Err: fmt.Errorf("error while removing previous multi route title: %w", err),
+			}
+			return
+		}
+	}
+	//「タイムスタンプを更新または追加」
+	err := model.UpdateMultiRouteTitles(r.userID, r.routesInfo.Title, "$set", now)
+	if err != nil {
+		r.err = customerr.BaseErr{
+			Op:  "Set new multi route title and timestamp",
+			Msg: "エラーが発生しました。",
+			Err: fmt.Errorf("error while setting new multi route title and timestamp: %w", err),
+		}
+		return
+	}
+}
+
 
 //ルートを更新保存するための関数
 func UpdateRoute(w http.ResponseWriter, req *http.Request) {
-	msg := "エラーが発生しました。もう一度操作を行ってください。"
-	//バリデーション完了後のrequestFieldsを取得
-	reqFields, ok := req.Context().Value("reqFields").(model.RouteUpdateRequest)
-	if !ok {
-		http.Error(w, msg, http.StatusInternalServerError)
-		log.Printf("Error while getting request fields from reuest's context: %v", ok)
-		return
-	}
+	//バリデーション完了後のルート情報と変更前のタイトルを取得
+	r, pTitle := getUpdateRoutesInfo(req)
 	//Auth middlewareからuserIDを取得
-	user, ok := req.Context().Value("user").(model.UserData)
-	if !ok {
-		http.Error(w, msg, http.StatusInternalServerError)
-		log.Printf("Error while getting userID from reuest's context: %v", ok)
-		return
-	}
+	r.getUserID(req)
+	//ルートの更新
+	r.updateRoute()
+	//ルート名とタイムスタンプの更新
+	r.updateRouteTitles(pTitle)
 
-	var err error
-
-	//routes collectionに保存
-	routeDoc := bson.M{"_id": reqFields.ID}
-	updateDoc := bson.D{
-		{"title", reqFields.Title},
-		{"routes", reqFields.Routes},
-	}
-	err = dbhandler.UpdateOne("googroutes", "routes", "$set", routeDoc, updateDoc)
-	if err != nil {
-		if strings.Contains(err.Error(), "(BSONObjectTooLarge)") {
-			msg = "ルートのデータサイズが大きすぎるため、保存できません。\nルートの分割をお願いします。"
-			http.Error(w, msg, http.StatusInternalServerError)
-			log.Printf("Error while updating multi route: %v", err)
-			return
-		}
-		http.Error(w, msg, http.StatusInternalServerError)
-		log.Printf("Error while updating multi route: %v", err)
-		return
-	}
-
-
-	userID := user.ID
-
-	//編集するルートのユーザー
-	userDoc := bson.M{"_id": userID}
-
-	if reqFields.Title == reqFields.PreviousTitle {
-		//「タイムスタンプを更新」
-		err = updateUsersRouteTitles(userID, reqFields.Title, "$set")
-	} else {
-		//「元のルート名をuser documentから削除」
-		deleteField := bson.M{"multi_route_titles." + reqFields.PreviousTitle: ""}
-		//documentではなく、document内のフィールドを削除する場合、Deleteではなく、Update operatorの$unsetを使って削除する
-		//公式ドキュメントURL: https://docs.mongodb.com/manual/reference/operator/update/unset/
-		err = dbhandler.UpdateOne("googroutes", "users", "$unset", userDoc, deleteField)
-
-		//「新しいルート名とタイムスタンプを追加」
-		err = updateUsersRouteTitles(userID, reqFields.Title, "$set")
-	}
-
-	if err != nil {
-		http.Error(w, msg, http.StatusInternalServerError)
-		log.Printf("Error while updating multi route titles in user document field: %v", err)
-		return
+	if r.err != nil {
+		e := r.err.(customerr.BaseErr)
+		http.Error(w, e.Msg, http.StatusInternalServerError)
+		log.Printf("operation: %s, error: %v", e.Op, e.Err)
 	}
 
 	//レスポンス作成
 	w.Header().Set("Content-Type", "application/json")
-	respMsg := ResponseMsg{Msg: "OK"}
-	respJson, err := json.Marshal(respMsg)
-	if err != nil {
-		log.Printf("Error while json marshaling: %v", err)
-	}
-	w.Write(respJson)
+	respMsg := map[string]string{"msg": "ok"}
+	json.NewEncoder(w).Encode(&respMsg)
 }
