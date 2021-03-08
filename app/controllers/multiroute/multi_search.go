bc6159e5c8d233e810b0b60c2eeb5e3ed248e0b0
heyar341 <kityonya@gmail.com>
2021-03-08 14:15:12 +0900

    プロセスごとにメソッドに分割して可読性を改善。また、エラーハンドリングを１カ所に集約

--- app/controllers/multiroute/multi_search.go
+++ app/controllers/multiroute/multi_search.go
@@ -1,10 +1,9 @@
 package multiroute
 
 import (
-	"app/dbhandler"
 	"app/model"
 	"encoding/json"
-	"go.mongodb.org/mongo-driver/bson"
+	"fmt"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"log"
 	"net/http"
@@ -12,78 +11,109 @@ import (
 	"time"
 )
 
-type ResponseMsg struct {
-	Msg string `json:"msg"`
+type routesData struct {
+	routesInfo model.MultiRoute
+	userID     primitive.ObjectID
+	err        error
 }
 
-//requestのフィールドを保存する変数
-type MultiSearchRequest struct {
-	Title  string                 `json:"title" bson:"title"`
-	Routes map[string]interface{} `json:"routes" bson:"routes"`
+type multiSearchErr struct {
+	Op  string
+	Msg string
+	Err error
 }
 
-func SaveRoutes(w http.ResponseWriter, req *http.Request) {
-	msg := "エラーが発生しました。"
-	//バリデーション完了後のrequestFieldsを取得
-	reqFields, ok := req.Context().Value("reqFields").(MultiSearchRequest)
+func (s multiSearchErr) Error() string {
+	return s.Err.Error()
+}
+
+func getRoutesInfo(req *http.Request) *routesData {
+	reqFields, ok := req.Context().Value("reqFields").(model.MultiRoute)
 	if !ok {
-		http.Error(w, msg, http.StatusInternalServerError)
-		log.Printf("Error while getting request fields from reuest's context: %v", ok)
+		return &routesData{
+			err: multiSearchErr{
+				Op:  "Get req routes info from context",
+				Msg: "エラーが発生しました。",
+				Err: fmt.Errorf("error while getting request fields from reuest's context"),
+			},
+		}
+	}
+	return &routesData{
+		routesInfo: reqFields,
+		err:        nil,
+	}
+}
+
+func (r *routesData) getUserID(req *http.Request) {
+	if r.err != nil {
 		return
 	}
-	//Auth middlewareからuserIDを取得
 	user, ok := req.Context().Value("user").(model.UserData)
 	if !ok {
-		http.Error(w, msg, http.StatusInternalServerError)
-		log.Printf("Error while getting userID from reuest's context: %v", ok)
-		return
-	}
-	userID := user.ID
-
-	/*users collectionのmulti_route_titlesフィールドにルート名と作成時刻を追加($set)する。
-	作成時刻はルート名取得時に作成時刻でソートするため*/
-	err := updateUsersRouteTitles(userID, reqFields.Title, "$set")
-	if err != nil {
-		http.Error(w, msg, http.StatusInternalServerError)
-		log.Printf("Error while saving multi route: %v", err)
-		return
-	}
-
-	//routes collectionに保存
-	routeDocument := bson.D{
-		{"user_id", userID},
-		{"title", reqFields.Title},
-		{"routes", reqFields.Routes},
-	}
-
-	_, err = dbhandler.Insert("googroutes", "routes", routeDocument)
-	if err != nil {
-		if strings.Contains(err.Error(), "(BSONObjectTooLarge)") {
-			msg = "ルートのデータサイズが大きすぎるため、保存できません。\nルートの分割をお願いします。"
-			http.Error(w, msg, http.StatusInternalServerError)
-			log.Printf("Error while saving multi route: %v", err)
-			return
+		r.err = multiSearchErr{
+			Op:  "Get user from context",
+			Msg: "エラーが発生しました。",
+			Err: fmt.Errorf("error while getting user from reuest's context"),
 		}
-		http.Error(w, msg, http.StatusInternalServerError)
-		log.Printf("Error while saving multi route: %v", err)
 		return
+	}
+	r.userID = user.ID
+}
+
+func (r *routesData) saveRoute() {
+	err := model.SaveRoute(r.userID, &r.routesInfo)
+	if err != nil {
+		e := multiSearchErr{
+			Op:  "Save new multi route",
+			Err: fmt.Errorf("error while saving multi route: %w", err),
+		}
+
+		if strings.Contains(err.Error(), "(BSONObjectTooLarge)") {
+			e.Msg = "ルートのデータサイズが大きすぎるため、保存できません。\nルートの分割をお願いします。"
+			r.err = e
+		} else {
+			e.Msg = "エラーが発生しました。"
+			r.err = e
+		}
+	}
+}
+
+//user documentのmult_route_titlesに新しいルート名とタイムスタンプを追加する関数
+func (r *routesData) updateRouteTitles() {
+	if r.err != nil {
+		return
+	}
+	now := time.Now().UTC() //MongoDBでは、timeはUTC表記で扱われ、タイムゾーン情報は入れられない
+	err := model.UpdateMultiRouteTitles(r.userID, r.routesInfo.Title, "$set", now)
+	if err != nil {
+		r.err = multiSearchErr{
+			Op:  "update user document's multi_route_titles field",
+			Msg: "エラーが発生しました。",
+			Err: fmt.Errorf("error while updating user's multi_route_titles %w", err),
+		}
+		return
+	}
+}
+
+func SaveNewRoute(w http.ResponseWriter, req *http.Request) {
+	//バリデーション完了後のrequest Fieldsを取得
+	r := getRoutesInfo(req)
+	//Auth middlewareからuserIDを取得
+	r.getUserID(req)
+	//routes collectionに保存
+	r.saveRoute()
+	/*users collectionのmulti_route_titlesフィールドにルート名と作成時刻を追加($set)する。
+	  作成時刻はルート名取得時に作成時刻でソートするため*/
+	r.updateRouteTitles()
+
+	if r.err != nil {
+		e := r.err.(multiSearchErr)
+		http.Error(w, e.Msg, http.StatusInternalServerError)
+		log.Printf("operation: %s, error: %v", e.Op, e.Err)
 	}
 
 	//レスポンス作成
 	w.Header().Set("Content-Type", "application/json")
-	msgJSON := ResponseMsg{Msg: "OK"}
-	respJson, err := json.Marshal(msgJSON)
-	if err != nil {
-		log.Printf("Error while json marshaling: %v", err)
-	}
-	w.Write(respJson)
-}
-
-//user documentのmult_route_titlesに新しいルート名とタイムスタンプを追加する関数
-func updateUsersRouteTitles(userID primitive.ObjectID, routeTitle, operator string) error {
-	userDoc := bson.M{"_id": userID}
-	now := time.Now().UTC()                                        //MongoDBでは、timeはUTC表記で扱われ、タイムゾーン情報は入れられない
-	updateField := bson.M{"multi_route_titles." + routeTitle: now} //nested fieldsは.(ドット表記)で繋いで書く
-	err := dbhandler.UpdateOne("googroutes", "users", operator, userDoc, updateField)
-	return err
+	msgJSON := map[string]string{"msg": "OK"}
+	json.NewEncoder(w).Encode(&msgJSON)
 }
