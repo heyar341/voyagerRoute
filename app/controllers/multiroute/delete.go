27c1177ffaf4cefeb9034677e9a92f19fc430846
heyar341 <kityonya@gmail.com>
2021-03-08 19:43:20 +0900

    操作ごとにメソッド分割して可読性改善。エラーハンドリングを１カ所に集約

--- app/controllers/multiroute/delete.go
+++ app/controllers/multiroute/delete.go
@@ -1,57 +1,86 @@
 package multiroute
 
 import (
-	"app/dbhandler"
+	"app/customerr"
 	"app/model"
-	"go.mongodb.org/mongo-driver/bson"
+	"fmt"
 	"log"
 	"net/http"
 )
 
-func DeleteRoute(w http.ResponseWriter, req *http.Request) {
-	if req.Method != "POST" {
-		msg := "HTTPメソッドが不正です。"
-		http.Redirect(w, req, "/mypage/show_routes/?msg="+msg, http.StatusSeeOther)
+type deleteRoute struct {
+	user       model.UserData
+	routeTitle string
+	err        error
+}
+
+func (d *deleteRoute) getUserFromCtx(req *http.Request) {
+	if d.err != nil {
 		return
 	}
-	msg := "エラーが発生しました。もう一度操作を行ってください。"
-	//Auth middlewareからuserIDを取得
 	user, ok := req.Context().Value("user").(model.UserData)
 	if !ok {
-		http.Redirect(w, req, "/mypage/show_routes/?msg="+msg, http.StatusSeeOther)
-		log.Printf("Error while getting userID from reuest's context: %v", ok)
+		d.err = customerr.BaseErr{
+			Op:  "Getting user from context",
+			Msg: "エラーが発生しました。",
+			Err: fmt.Errorf("error while getting user from reuest's context"),
+		}
 		return
 	}
-	userID := user.ID
+	d.user = user
+}
 
-	routeTitle := req.FormValue("title")
-
-	//削除するルートのユーザー
-	userDoc := bson.M{"_id": userID}
-
-	//「元のルート名をuser documentから削除」
-	deleteField := bson.M{"multi_route_titles." + routeTitle: ""}
-	//documentではなく、document内のフィールドを削除する場合、Deleteではなく、Update operatorの$unsetを使って削除する
-	//公式ドキュメントURL: https://docs.mongodb.com/manual/reference/operator/update/unset/
-	err := dbhandler.UpdateOne("googroutes", "users", "$unset", userDoc, deleteField)
+func (d *deleteRoute) deleteRoute() {
+	if d.err != nil {
+		return
+	}
+	err := model.UpdateMultiRouteTitles(d.user.ID, d.routeTitle, "$unset", "")
 	if err != nil {
-		http.Redirect(w, req, "/mypage/show_routes/?msg="+msg, http.StatusSeeOther)
-		log.Printf("Error while deleting %v from multi_route_titles: %v", routeTitle, err)
+		d.err = customerr.BaseErr{
+			Op:  "Deleting route title",
+			Msg: "エラーが発生しました。",
+			Err: fmt.Errorf("error while deleting %v from multi_route_titles: %w", d.routeTitle, err),
+		}
 		return
 	}
+}
 
-	////routes collectionから削除 エラー解析に使うかもしれないので、削除せずに残しておく
-	//routeDoc := bson.D{{"title", routeTitle}, {"user_id", userID}}
-	//err = dbhandler.Delete("googroutes", "routes", routeDoc)
-	//if err != nil {
-	//	msg = "エラ-が発生しました。もう一度操作をしなおしてください。"
-	//	http.Redirect(w, req, "/mypage/show_routes/?msg="+msg, http.StatusSeeOther)
-	//	log.Printf("Error while deleting route %v: %v",routeTitle, err)
-	//	return
-	//}
+func makeCookieAndRedirect(w http.ResponseWriter, req *http.Request, cName, cVal string) {
+	c := &http.Cookie{
+		Name:   cName,
+		Value:  cVal,
+		Path:   "/mypage/show_routes",
+		MaxAge: 5,
+	}
+	http.SetCookie(w, c)
+	http.Redirect(w, req, "/mypage/show_routes", http.StatusSeeOther)
+}
+
+func DeleteRoute(w http.ResponseWriter, req *http.Request) {
+	var d = deleteRoute{}
+	if req.Method != "POST" {
+		d.err = customerr.BaseErr{
+			Msg: "HTTPメソッドが不正です。",
+			Err: fmt.Errorf("invalid HTTP method access"),
+		}
+	}
+	//Auth middlewareからuserIDを取得
+	d.getUserFromCtx(req)
+	//requestから要挙するタイトルを取得
+	d.routeTitle = req.FormValue("title")
+	//「元のルート名をuser documentから削除」
+	d.deleteRoute()
 
 	//レスポンス作成
-	success := "ルート「" + routeTitle + "」を削除しました。"
-	http.Redirect(w, req, "/mypage/show_routes/?success="+success, http.StatusSeeOther)
-	log.Printf("User [%v] deleted route [%v]", user.UserName, routeTitle)
+	if d.err != nil {
+		e := d.err.(customerr.BaseErr)
+		makeCookieAndRedirect(w, req, "msg", e.Msg)
+		log.Printf("Error while deleting route title: %v", e.Err)
+		return
+	}
+
+	makeCookieAndRedirect(w, req, "success", "ルート「"+d.routeTitle+"」を削除しました。")
+	log.Printf("User [%v] deleted route [%v]", d.user.UserName, d.routeTitle)
 }
+
+////routes collectionから削除 エラー解析に使うかもしれないので、rout自体は削除せずに残しておく
