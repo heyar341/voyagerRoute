26cb81aa33bfcd65cb6d668248a3e71ce67bd072
heyar341 <kityonya@gmail.com>
2021-03-09 21:02:54 +0900

    Session modelを作成し、DB操作をmodel内に集約

---
+++ app/model/session.go
@@ -0,0 +1,28 @@
+package model
+
+import (
+	"app/dbhandler"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Session struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	SessionID string             `bson:"session_id"`
+	UserID    primitive.ObjectID `bson:"user_id"`
+}
+
+func DeleteSession(sessionID string) error {
+	d := bson.M{"session_id": sessionID}
+	err := dbhandler.Delete("googroutes", "sessions", d)
+	return err
+}
+
+func CreateNewSession(sessionID string, userID primitive.ObjectID) error {
+	d := bson.D{
+		{"session_id", sessionID},
+		{"user_id", userID},
+	}
+	_, err := dbhandler.Insert("googroutes", "sessions", d)
+	return err
+}
