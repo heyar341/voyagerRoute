123f39923f81c7d90e28a126489bcff43434bf73
heyar341 <kityonya@gmail.com>
2021-03-09 10:55:41 +0900

    FindUser関数を作成

--- app/model/user.go
+++ app/model/user.go
@@ -7,16 +7,24 @@ import (
 )
 
 type User struct {
-	ID               primitive.ObjectID   `json:"id" bson:"_id"`
-	UserName         string               `json:"username" bson:"username"`
-	Email            string               `json:"email" bson:"email"`
-	Password         []byte               `json:"password" bson:"password"`
+	ID       primitive.ObjectID `json:"id" bson:"_id"`
+	UserName string             `json:"username" bson:"username"`
+	Email    string             `json:"email" bson:"email"`
+	Password []byte             `json:"password" bson:"password"`
 	//MultiRouteTitles map[string]time.Time `json:"multi_route_titles" bson:"multi_route_titles"`
 }
 
 func UpdateMultiRouteTitles(userID primitive.ObjectID, routeTitle, operator string, updateVal interface{}) error {
-		userDoc := bson.M{"_id": userID}
-		updateField := bson.M{"multi_route_titles." + routeTitle: updateVal} //nested fieldsは.(ドット表記)で繋いで書く
-		err := dbhandler.UpdateOne("googroutes", "users", operator, userDoc, updateField)
-		return err
+	userDoc := bson.M{"_id": userID}
+	updateField := bson.M{"multi_route_titles." + routeTitle: updateVal} //nested fieldsは.(ドット表記)で繋いで書く
+	err := dbhandler.UpdateOne("googroutes", "users", operator, userDoc, updateField)
+	return err
+}
+
+func FindUser(fieldName, fieldValue string) (bson.M, error) {
+	//取得するドキュメントの条件
+	userDoc := bson.M{fieldName: fieldValue}
+	//DBから取得
+	d, err := dbhandler.Find("googroutes", "users", userDoc, nil)
+	return d, err
 }
