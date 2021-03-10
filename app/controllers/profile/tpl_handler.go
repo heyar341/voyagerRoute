a086dd59c4f4e0ad86914624e912ca01acfbff2c
heyar341 <kityonya@gmail.com>
2021-03-10 17:25:30 +0900

    toutil削除に伴い修正

--- app/controllers/profile/tpl_handler.go
+++ app/controllers/profile/tpl_handler.go
@@ -1,9 +1,10 @@
 package profile
 
 import (
+	"app/controllers"
 	"app/cookiehandler"
 	"app/customerr"
-	"app/tplutil"
+	"app/model"
 	"encoding/base64"
 	"html/template"
 	"log"
@@ -11,6 +12,12 @@ import (
 )
 
 var profileTpl *template.Template
+
+type tplProcess struct {
+	data map[string]interface{}
+	user model.User
+	err  error
+}
 
 func init() {
 	profileTpl = template.Must(template.Must(template.ParseGlob("templates/profile/*.html")).ParseGlob("templates/includes/*.html"))
@@ -27,71 +34,79 @@ func processCookie(w http.ResponseWriter, c *http.Cookie, data map[string]interf
 }
 
 func ShowProfile(w http.ResponseWriter, req *http.Request) {
-	t := tplutil.GetTplData(req)
-	if t.Err != nil {
-		e := t.Err.(customerr.BaseErr)
+	var t tplProcess
+	t.data = controllers.GetLoginDataFromCtx(req)
+	t.user, t.err = controllers.GetUserFromCtx(req)
+	if t.err != nil {
+		e := t.err.(customerr.BaseErr)
 		cookiehandler.MakeCookieAndRedirect(w, req, "msg", e.Msg, "/mypage")
 		log.Printf("operation: %s, error: %v", e.Op, e.Err)
 		return
 	}
-	t.Data["userName"] = t.User.UserName
-	t.Data["email"] = t.User.Email
+	t.data["userName"] = t.user.UserName
+	t.data["email"] = t.user.Email
 	c, err := req.Cookie("success")
 	if err == nil {
-		processCookie(w, c, t.Data, "profile.html")
+		processCookie(w, c, t.data, "profile.html")
 		return
 	}
 	c, err = req.Cookie("msg")
 	if err == nil {
-		processCookie(w, c, t.Data, "profile.html")
+		processCookie(w, c, t.data, "profile.html")
 		return
 	}
 
-	profileTpl.ExecuteTemplate(w, "profile.html", t.Data)
+	profileTpl.ExecuteTemplate(w, "profile.html", t.data)
 }
 
 func EditUserNameForm(w http.ResponseWriter, req *http.Request) {
-	t := tplutil.GetTplData(req)
-	if t.Err != nil {
-		e := t.Err.(customerr.BaseErr)
+	var t tplProcess
+	t.data = controllers.GetLoginDataFromCtx(req)
+	t.user, t.err = controllers.GetUserFromCtx(req)
+	if t.err != nil {
+		e := t.err.(customerr.BaseErr)
 		cookiehandler.MakeCookieAndRedirect(w, req, "msg", e.Msg, "/mypage")
 		log.Printf("operation: %s, error: %v", e.Op, e.Err)
 		return
 	}
-	t.Data["userName"] = t.User.UserName
+	t.data["userName"] = t.user.UserName
 	c, err := req.Cookie("msg")
 	if err == nil {
-		processCookie(w, c, t.Data, "username_edit.html")
+		processCookie(w, c, t.data, "username_edit.html")
 		return
 	}
 
-	profileTpl.ExecuteTemplate(w, "username_edit.html", t.Data)
+	profileTpl.ExecuteTemplate(w, "username_edit.html", t.data)
 }
 func EditEmailForm(w http.ResponseWriter, req *http.Request) {
-	t := tplutil.GetTplData(req)
-	if t.Err != nil {
-		e := t.Err.(customerr.BaseErr)
+	var t tplProcess
+	t.data = controllers.GetLoginDataFromCtx(req)
+	t.user, t.err = controllers.GetUserFromCtx(req)
+	if t.err != nil {
+		e := t.err.(customerr.BaseErr)
 		cookiehandler.MakeCookieAndRedirect(w, req, "msg", e.Msg, "/mypage")
 		log.Printf("operation: %s, error: %v", e.Op, e.Err)
 		return
 	}
-	t.Data["email"] = t.User.Email
+	t.data["email"] = t.user.Email
 	newEmail := req.URL.Query().Get("newEmail")
-	t.Data["newEmail"] = newEmail
+	t.data["newEmail"] = newEmail
 
 	c, err := req.Cookie("msg")
 	if err == nil {
-		processCookie(w, c, t.Data, "email_edit.html")
+		processCookie(w, c, t.data, "email_edit.html")
 		return
 	}
 
-	profileTpl.ExecuteTemplate(w, "email_edit.html", t.Data)
+	profileTpl.ExecuteTemplate(w, "email_edit.html", t.data)
 }
 
 func EditPasswordForm(w http.ResponseWriter, req *http.Request) {
-	t := tplutil.GetTplData(req)
-	if t.Err != nil {
-		e := t.Err.(customerr.BaseErr)
+	var t tplProcess
+	t.data = controllers.GetLoginDataFromCtx(req)
+	t.user, t.err = controllers.GetUserFromCtx(req)
+	if t.err != nil {
+		e := t.err.(customerr.BaseErr)
 		cookiehandler.MakeCookieAndRedirect(w, req, "msg", e.Msg, "/mypage")
 		log.Printf("operation: %s, error: %v", e.Op, e.Err)
 		return
@@ -99,8 +114,8 @@ func EditPasswordForm(w http.ResponseWriter, req *http.Request) {
 
 	c, err := req.Cookie("msg")
 	if err == nil {
-		processCookie(w, c, t.Data, "password_edit.html")
+		processCookie(w, c, t.data, "password_edit.html")
 		return
 	}
-	profileTpl.ExecuteTemplate(w, "password_edit.html", t.Data)
+	profileTpl.ExecuteTemplate(w, "password_edit.html", t.data)
 }
