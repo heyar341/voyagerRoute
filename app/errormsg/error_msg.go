6af188844791b248ae021158a1928a5e2d643731
heyar341 <kityonya@gmail.com>
2022-01-07 22:52:06 +0900

    DBに保存できない時のエラーメッセージを追加

--- app/errormsg/error_msg.go
+++ app/errormsg/error_msg.go
@@ -1,6 +1,7 @@
 package errormsg
 
 const (
-	WrongMailOrPwd = "メールアドレスまたはパスワードが間違っています。"
-	SomethingBad   = "エラーが発生しました。"
+	WrongMailOrPwd    = "メールアドレスまたはパスワードが間違っています。"
+	SomethingBad      = "エラーが発生しました。"
+	RouteDataTooLarge = "ルートのデータサイズが大きすぎるため、保存できません。\nルートの分割をお願いします。"
 )
