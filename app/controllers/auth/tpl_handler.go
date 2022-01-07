41532a2c42bbb755761a041166ca5b649cb67738
heyar341 <kityonya@gmail.com>
2022-01-07 19:07:14 +0900

    不要になった関数削除

--- app/controllers/auth/tpl_handler.go
+++ app/controllers/auth/tpl_handler.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-	"app/contexthandler"
 	"encoding/base64"
 	"html/template"
 	"net/http"
@@ -13,42 +12,12 @@ func init() {
 	authTpl = template.Must(template.Must(template.ParseGlob("templates/auth/*.html")).ParseGlob("templates/includes/*.html"))
 }
 
-func processCookie(w http.ResponseWriter, c *http.Cookie, loginState map[string]interface{}, tplName string) {
+func tplWithCookieMsg(w http.ResponseWriter, c *http.Cookie, data map[string]interface{}, tplName string) {
 	b64Str, err := base64.StdEncoding.DecodeString(c.Value)
 	if err != nil {
-		authTpl.ExecuteTemplate(w, tplName, loginState)
+		authTpl.ExecuteTemplate(w, tplName, data)
 		return
 	}
-	loginState[c.Name] = string(b64Str)
-	authTpl.ExecuteTemplate(w, tplName, loginState)
-}
-
-func AskConfirmEmail(w http.ResponseWriter, req *http.Request) {
-	loginState := contexthandler.GetLoginStateFromCtx(req)
-	authTpl.ExecuteTemplate(w, "ask_confirm_email.html", loginState)
-}
-
-func RegisterForm(w http.ResponseWriter, req *http.Request) {
-	loginState := contexthandler.GetLoginStateFromCtx(req)
-	c, err := req.Cookie("msg")
-	if err == nil {
-		processCookie(w, c, loginState, "register.html")
-		return
-	}
-	authTpl.ExecuteTemplate(w, "register.html", loginState)
-}
-
-func LoginForm(w http.ResponseWriter, req *http.Request) {
-	loginState := contexthandler.GetLoginStateFromCtx(req)
-	c, err := req.Cookie("msg")
-	if err == nil {
-		processCookie(w, c, loginState, "login.html")
-		return
-	}
-	c, err = req.Cookie("success")
-	if err == nil {
-		processCookie(w, c, loginState, "login.html")
-		return
-	}
-	authTpl.ExecuteTemplate(w, "login.html", loginState)
+	data[c.Name] = string(b64Str)
+	authTpl.ExecuteTemplate(w, tplName, data)
 }
