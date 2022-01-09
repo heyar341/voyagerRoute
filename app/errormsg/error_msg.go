f92049acd852191e00611e74c46bd086885878ad
heyar341 <kityonya@gmail.com>
2022-01-09 21:19:27 +0900

    エラーメッセージを追加

--- app/errormsg/error_msg.go
+++ app/errormsg/error_msg.go
@@ -3,4 +3,6 @@ package errormsg
 const (
 	WrongMailOrPwd = "メールアドレスまたはパスワードが間違っています。"
 	SomethingBad   = "エラーが発生しました。"
+	TriAgain       = "エラーが発生しました。しばらく経ってからもう一度ご利用ください。"
+	TooLargeData   = "ルートのデータサイズが大きすぎるため、保存できません。\nルートの分割をお願いします。"
 )
