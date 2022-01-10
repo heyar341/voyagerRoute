da49fd05b7dc7f19d744b41e7d49e8f2e963e373
heyar341 <kityonya@gmail.com>
2022-01-10 11:03:41 +0900

    conflict解消

--- app/errormsg/error_msg.go
+++ app/errormsg/error_msg.go
@@ -3,6 +3,6 @@ package errormsg
 const (
 	WrongMailOrPwd    = "メールアドレスまたはパスワードが間違っています。"
 	SomethingBad      = "エラーが発生しました。"
-	TriAgain          = "エラーが発生しました。しばらく経ってからもう一度ご利用ください。"
 	RouteDataTooLarge = "ルートのデータサイズが大きすぎるため、保存できません。\nルートの分割をお願いします。"
+	TriAgain          = "エラーが発生しました。しばらく経ってからもう一度ご利用ください。"
 )
